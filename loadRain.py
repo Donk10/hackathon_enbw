3874c0c5dc2e4112ca22c413836d44f9f19f5b82
Luca Blessing <luca@Lucas-MacBook-Pro.local>
2018-11-05 20:25:22 +0100

    all in one function

--- loadRain.py
+++ loadRain.py
@@ -10,8 +10,32 @@ def loadRainData():
     # store data and description in 2 arrays
     description = file_in_npArray[0,1:4]
     rainValue_mm = np.array(file_in_npArray[1:,2:4], dtype=float)
-    # return raind data as numpy array
-    return rainValue_mm
+
+    daysInMonth = np.array([31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31])
+    months = np.arange(1,13,1, dtype=int)
+    hours = np.arange(0,24,1, dtype=int)
+
+    timeStamps = np.array([])
+
+    start2017 = 201712010000
+    end2017 = 201712312300
+    start2018 = 201801010000
+    end2018 = 201808012200
+
+    for year in [2017,2018]:
+        for month in months:
+            for day in range(1,1+daysInMonth[month-1]):
+                for hour in hours:
+                    temporaryTimeStamp = year*10**8 + month*10**6 + day*10**4 + hour*10**2
+                    if temporaryTimeStamp >= start2017 and temporaryTimeStamp <= end2017:
+                        timeStamps = np.append(timeStamps, temporaryTimeStamp)
+                    if temporaryTimeStamp >= start2018 and temporaryTimeStamp <= end2018:
+                        timeStamps = np.append(timeStamps, temporaryTimeStamp)
+
+    newValueArray = np.zeros_like(timeStamps)
+    for i in range(timeStamps.shape[0]):
+        newValueArray[i] = rainValue_mm[np.where(rainValue_mm[:,0] == timeStamps[i])[0][0],1]
+    return newValueArray
 
 '''
 Function parameters, intervalls, integer:
